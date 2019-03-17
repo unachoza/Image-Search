d942ebfe079ca633a0a9b4c73d6447b9b3584a2c
Arianna Choza <unachoza@gmail.com>
2019-03-17 09:36:54 -0400

    initialized state and created form with eventhandlers for input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react'
 
 class SearchBar extends Component{
+    constructor(props){
+        super(props)
+        this.state = { 
+            term : ""
+        }
+    }
+    
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state.term)
+    }
     render(){
         return(
             <div className="ui segment">
-                <form className="ui form">
+                <form 
+                onSubmit={this.handleSubmit}
+                className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input 
+                        type="text"
+                        value={this.state.term}
+                        onChange={e => this.setState({term: e.target.value})}/>
                     </div>
                 </form>
             </div>
