d53dbcc179be107eb7456b4972441de6f2c88896
Arianna Choza <unachoza@gmail.com>
2019-03-18 09:59:03 -0400

    axios get method from unsplashed api. Search renders lenght of results array

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, {Component} from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{marginTop: '10px'}}>
-        <SearchBar />
-        </div>
-        )
+class App extends Component{
+    state = { images: [] }
+ 
+    
+    
+
+     onSearchSubmit = async(term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 6b7198ae94debdaaa4a572ff5548c06da0c748f53dd039de22f8d80eb85188b7'
+            }
+        })
+
+        this.setState({ images : response.data.results})
+    }
+
+    render(){
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found {this.state.images.length} images
+            </div>
+            )
+    }
+    
+
 }
+ 
 
 export default App
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,25 @@
 import React, {Component} from 'react'
 
+
 class SearchBar extends Component{
     constructor(props){
         super(props)
         this.state = { 
             term : ""
-        }
+        } 
+        // this.handleFormSubmit = this.handleFormSubmit.bind(this)
     }
     
 
-    handleSubmit = (e) => {
+    handleFormSubmit = (e) => {
         e.preventDefault()
-        console.log(this.state.term)
+        this.props.onSubmit(this.state.term)
     }
     render(){
         return(
             <div className="ui segment">
                 <form 
-                onSubmit={this.handleSubmit}
+                onSubmit={this.handleFormSubmit}
                 className="ui form">
                     <div className="field">
                         <label>Image Search</label>
